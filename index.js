9d939b9048837d24df8a7d14f8148380d29e9603
Daniel Cotic <d.cotic26@gmail.com>
2017-12-13 03:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+    var name = "Ralph"
+    kittens.push(name)
+    return kittens
+}
+
+function destructivelyPrependKitten(name){
+    var name = "Bob"
+    kittens.unshift(name)
+    return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+    kittens.pop()
+    return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+    kittens.shift()
+    return kittens
+}
+
+function appendKitten(name){
+  var name = "Broom"
+  var newArray = [...kittens,name]
+  return newArray
+}
+
+function prependKitten(name){
+  var name = "Arnold"
+  var newArray = [name,...kittens]
+  return newArray
+}
+
+function removeLastKitten(){
+  var newArray = kittens.slice(0,kittens.length -1)
+  return newArray
+}
